7cc4b0b23ada875ca9e8833180b668d82d57bd99
dd0s4c1d <114265164+dd0s4c1d@users.noreply.github.com>
2025-04-13 10:16:55 -0400

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 // https://astro.build/config
-export default defineConfig({});
+import icon from "astro-icon";
+
+export default defineConfig({
+  integrations: [icon()],
+});
